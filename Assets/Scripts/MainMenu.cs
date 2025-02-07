79ad82de47cabf9150ba815512dfdadc6debf981
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-07 15:09:38 +0200

    Added comments in some scripts

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,21 +9,19 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
- 
     public GameObject panel;
     TextMeshProUGUI levelText;
+
+    // Method to start the game. It loads the next scene in the build index.
     public void PlayGame()
     {
-
+        // Load the next scene based on the current scene's build index.
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-     
-        
     }
 
-
+    // Method to quit the game. Works only in a built application (not in the Unity Editor).
     public void QuitGame ()
     {
-    
-        Application.Quit();
+        Application.Quit(); // Quits the application.
     }
 }
--- Assets/Scripts/backToMenu.cs
+++ Assets/Scripts/backToMenu.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// This script handles returning to the main menu in a Unity project.
 public class backToMenu : MonoBehaviour
 {
+    // This method is triggered when the "Back to Menu" button is pressed.
     public void BacktoMenuButton()
     {
+        // Loads the scene at index 0, the main menu scene.
         SceneManager.LoadScene(0);
     }
 }
